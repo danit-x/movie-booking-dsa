bb6dc584b1c2e08135873c8f0b819b607e2efa6a
D.P.S.D. Tharakie <eg255918@engug.ruh.ac.lk>
2026-04-27 17:20:52 +0530

    BST Moving title create

---
+++ src/BSTMovieTitle.cpp
@@ -0,0 +1,132 @@
+#include <cstring>
+
+#include "BSTMovieTitle.h"
+
+BSTMovieTitle::BSTMovieTitle()
+{
+    root = NULL;
+    size = 0;
+}
+
+BSTMovieTitle::~BSTMovieTitle()
+{
+    clear();
+}
+
+void BSTMovieTitle::insert(int movieId, const std::string &title)
+{
+    Node *newNode = new Node;
+    newNode->movieId = movieId;
+    newNode->title = title;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    newNode->parent = NULL;
+
+    if (root == NULL)
+    {
+        root = newNode;
+        size++;
+        return;
+    }
+
+    Node *current = root;
+    Node *parentNode = NULL;
+    int compareResult = 0;
+
+    while (current != NULL)
+    {
+        parentNode = current;
+        if (title < current->title)
+            compareResult = -1;
+        else if (title > current->title)
+            compareResult = 1;
+        else
+            compareResult = 0;
+
+        if (compareResult < 0)
+        {
+            current = current->left;
+        }
+        else
+        {
+            current = current->right;
+        }
+    }
+
+    newNode->parent = parentNode;
+    if (compareResult < 0)
+    {
+        parentNode->left = newNode;
+    }
+    else
+    {
+        parentNode->right = newNode;
+    }
+
+    size++;
+}
+
+int BSTMovieTitle::searchExact(const std::string &title) const
+{
+    Node *current = root;
+    while (current != NULL)
+    {
+        if (title == current->title)
+        {
+            return current->movieId;
+        }
+        if (title < current->title)
+        {
+            current = current->left;
+        }
+        else
+        {
+            current = current->right;
+        }
+    }
+
+    return -1;
+}
+
+void BSTMovieTitle::inOrderList(int outIds[], int &count) const
+{
+    count = 0;
+    inOrderFill(root, outIds, count);
+}
+
+void BSTMovieTitle::clear()
+{
+    clearNode(root);
+    root = NULL;
+    size = 0;
+}
+
+int BSTMovieTitle::getSize() const
+{
+    return size;
+}
+
+void BSTMovieTitle::clearNode(Node *node)
+{
+    if (node == NULL)
+    {
+        return;
+    }
+
+    clearNode(node->left);
+    clearNode(node->right);
+    delete node;
+}
+
+void BSTMovieTitle::inOrderFill(Node *node, int outIds[], int &count) const
+{
+    if (node == NULL)
+    {
+        return;
+    }
+
+    inOrderFill(node->left, outIds, count);
+    outIds[count] = node->movieId;
+    count++;
+    inOrderFill(node->right, outIds, count);
+}
